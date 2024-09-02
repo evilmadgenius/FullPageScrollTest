bad1c4a67147fb4a21f2f9e6e586efe178fd0992
zeeshan <zeeshan.ali.one@gmail.com>
2024-09-02 21:55:05 +0530

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,28 +6,21 @@ import { AppComponent } from './app.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { RouterModule } from '@angular/router';
 import { FileService } from './services/file.service';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HttpService } from './services/HttpService.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {ReactiveFormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    AboutmeComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-      { path: '', component: AppComponent },
-      { path: 'aboutme', component: AboutmeComponent },
-    ]),
-    HttpClientModule,
-    BrowserAnimationsModule, MatCardModule,ReactiveFormsModule,MatInputModule
-  ],
-  providers: [FileService,HttpService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        AboutmeComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        RouterModule.forRoot([
+            { path: '', component: AppComponent },
+            { path: 'aboutme', component: AboutmeComponent },
+        ]),
+        BrowserAnimationsModule, MatCardModule, ReactiveFormsModule, MatInputModule], providers: [FileService, HttpService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
